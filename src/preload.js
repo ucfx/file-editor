50510c580977903154f2d58ae57a5ad877f1f08d
youcefhmd <65844371+youcefhmd@users.noreply.github.com>
2023-11-02 20:34:45 +0100

    Duplicate file

--- src/preload.js
+++ src/preload.js
@@ -14,11 +14,20 @@ window.addEventListener("DOMContentLoaded", () => {
     );
     const btnConfirm = document.getElementById("btn-confirm");
     const btnCancel = document.getElementById("btn-cancel");
+    const btnDublicatedFiles = document.getElementById(
+        "btn-get-dublicated-files"
+    );
+    const btnInputDuplicate = document.getElementById("input-check-dublicate");
 
     btnConfirm.addEventListener("click", (event) => {
         const checkedFiles = getCheckedFiles();
         if (checkedFiles.length === 0) return;
         ipcRenderer.send("delete-all-file-selected", checkedFiles);
+        if (btnInputDuplicate.checked) {
+            setTimeout(() => {
+                getDuplicateFile();
+            }, 50);
+        }
         removeShowClass();
     });
     btnCancel.addEventListener("click", (event) => {
@@ -39,6 +48,9 @@ window.addEventListener("DOMContentLoaded", () => {
     btnCloseFolder.addEventListener("click", (event) => {
         conatainer[0].classList.remove("selected");
         document.getElementById("directory-container").innerHTML = "";
+
+        directoryContainer.classList.remove("collapsed");
+        btnInputDuplicate.checked = false;
         directoryData = null;
     });
 
@@ -82,35 +94,47 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     searchInput.addEventListener("keyup", async (event) => {
+        btnInputDuplicate.checked = false;
         const searchValue = event.target.value;
         if (!directoryData) return;
         if (searchValue === "") {
             ipcRenderer.send("read-dir", dirPath);
-            directoryContainer.classList.remove("onsearch");
+            directoryContainer.classList.remove("collapsed");
             return;
         }
         const files = searchFiles(directoryData, searchValue);
         let dirData = await buildDirectoryStructure(files);
         console.log("dirDate", dirData);
         directoryContainer.innerHTML = "";
-        directoryContainer.classList.add("onsearch");
+        directoryContainer.classList.add("collapsed");
         createDirectoryStructure(directoryContainer, dirData);
         setEvents();
+        setDisplayBlock();
     });
 
-    function createDirectoryStructure(container, data, path = "") {
+    function createDirectoryStructure(
+        container,
+        data,
+        dublicate = false,
+        path = ""
+    ) {
+        console.log("path:::: ", data);
         const ul = document.createElement("ul");
 
+        if (dublicate) {
+            directoryContainer.classList.add("collapsed");
+            directoryContainer.classList.add("dublicated");
+        }
         for (const key in data) {
             const li = document.createElement("li");
             const span = document.createElement("span");
             const i = document.createElement("i");
-            const checkInput = document.createElement("input");
             const pathInput = document.createElement("input");
+            const checkInput = document.createElement("input");
 
             pathInput.setAttribute("type", "hidden");
             pathInput.setAttribute("name", "path");
-            pathInput.setAttribute("value", `${path}`);
+            pathInput.setAttribute("value", path);
 
             checkInput.setAttribute("type", "checkbox");
             checkInput.setAttribute("name", "check");
@@ -119,11 +143,11 @@ window.addEventListener("DOMContentLoaded", () => {
             checkInput.setAttribute("value", key);
 
             i.classList.add("fa-light");
-            i.classList.add("fa-file-alt");
+            i.classList.add(dublicate ? "fa-search" : "fa-file-alt");
             span.textContent = key;
 
             li.appendChild(pathInput);
-            li.appendChild(checkInput);
+            if (!dublicate) li.appendChild(checkInput);
             li.appendChild(i);
             li.appendChild(span);
             if (data[key]) {
@@ -133,7 +157,8 @@ window.addEventListener("DOMContentLoaded", () => {
                     createDirectoryStructure(
                         document.createElement("div"),
                         data[key],
-                        `${path}/${key}`
+                        false,
+                        `${dublicate ? "" : `${path}/${key}`}`
                     )
                 );
             }
@@ -182,6 +207,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     async function buildDirectoryStructure(filePaths) {
+        console.log("filePaths: ", filePaths);
         const directory = {};
 
         await Promise.all(
@@ -190,7 +216,6 @@ window.addEventListener("DOMContentLoaded", () => {
                 let currentLevel = directory;
                 await Promise.all(
                     pathComponents.map(async (component, index) => {
-                        console.log("Component: ", component);
                         if (!currentLevel[component]) {
                             if (index === pathComponents.length - 1) {
                                 if (
@@ -223,6 +248,88 @@ window.addEventListener("DOMContentLoaded", () => {
                         ? "block"
                         : "none";
             });
+        });
+    }
+
+    async function getDuplicateFile() {
+        if (!btnInputDuplicate.checked) {
+            directoryContainer.classList.remove("dublicated");
+            directoryContainer.classList.remove("collapsed");
+            directoryContainer.innerHTML = "";
+
+            createDirectoryStructure(directoryContainer, directoryData);
+            setEvents();
+            setDisplayNone();
+            return;
+        }
+
+        const dublicatedFiles = findDuplicates(directoryData);
+        console.log("dublicatedFiles:: ", dublicatedFiles);
+        dirStructure = {};
+
+        const promises = Object.keys(dublicatedFiles).map(async (key) => {
+            let dirData = {};
+            await Promise.all(
+                dublicatedFiles[key].map(async (item) => {
+                    item = item.split("/");
+                    item.pop();
+                    item = item.join("\\");
+                    console.log("item", item);
+                    dirData[item] = {};
+                    dirData[item][key] = null;
+                })
+            );
+            dirStructure[key] = dirData;
+            console.log(dirData);
+        });
+        await Promise.all(promises);
+
+        console.log("dirDate", dirStructure);
+        directoryContainer.innerHTML = "";
+        createDirectoryStructure(directoryContainer, dirStructure, true);
+        setEvents();
+        setDisplayBlock();
+        isDublicated = true;
+    }
+
+    btnInputDuplicate.addEventListener("change", getDuplicateFile);
+    const findDuplicates = (directoryData) => {
+        let filesPath = {};
+
+        const findFiles = (directoryData, path = "") => {
+            for (const key in directoryData) {
+                const newPath = `${path}/${key}`;
+
+                if (directoryData[key]) {
+                    findFiles(directoryData[key], newPath);
+                } else {
+                    filesPath[key] = [...(filesPath[key] || []), newPath];
+                }
+            }
+        };
+
+        findFiles(directoryData);
+
+        const dublicatedFiles = Object.assign({ ...filesPath });
+        Object.keys(dublicatedFiles).forEach((key) => {
+            if (Object.keys(dublicatedFiles[key]).length < 2) {
+                delete dublicatedFiles[key];
+            }
+        });
+        console.log("filesPath  ---: ", dublicatedFiles);
+
+        return { ...dublicatedFiles };
+    };
+
+    function setDisplayBlock() {
+        document.querySelectorAll("ul").forEach((ul) => {
+            ul.style.display = "block";
+        });
+    }
+
+    function setDisplayNone() {
+        document.querySelectorAll("ul>li>ul").forEach((ul) => {
+            ul.style.display = "none";
         });
     }
 });
